cff13b335a20416b492ee81433c4424bdab9a757
Kmbariah <Kmbaria@github.com>
2020-05-08 14:38:39 +0300

    ! changes on the manage.py

--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 from app import create_app ,db
 from flask_script import Manager,Server
 from app.models import User, Comment, Pitch
-from flask_migrate import Migrate , MigrateCommand
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app('production')
+
 
 
 manager = Manager(app)
